1455de0721a1e0e41d5eebdac00317777982065a
EndlessPath <rk.chensiyu@gmail.com>
2024-10-08 12:38:25 +0800

    Site updated: 2024-10-08 12:38:25

--- js/LabelPage.js
+++ js/LabelPage.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function() {
 	if (document.hidden) {
 		$('[rel="icon"]').attr('href', "/funny.ico");
-		document.title = '╭(°A°`)╮ 人生若只如初见';
+		document.title = '╭(°A°`)╮ 你不要我了吗 -';
 		clearTimeout(titleTime);
 	} else {
 		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
-		document.title = '(ฅ>ω<*ฅ)  既见君子 云胡不喜';
+		document.title = '(ฅ>ω<*ฅ) 欢迎回来';
 		titleTime = setTimeout(function() {
 			document.title = OriginTitle;
 		}, 2000);
